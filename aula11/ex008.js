14857d43e76d64a25927bba5e2318699a2773c6a
henrique <https://github.com/geviewski>
2021-04-16 20:57:12 -0300

    ex11

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 58
+console.log(`A velocidade o seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Voce está MULTADO! voce passou do limete de velocidade!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+console.log('-------------------------------------')
+var n1 = 2
+var n2 = 3 
+var s = n1 + n2
+console.log(`A soma entre ${n1} e ${n2} é igual a ${s}`)
